fb5a896ce186eed443c11d57cf17159d95021d3e
Yin Sanders <yinsanders@gmail.com>
2024-08-18 23:45:35 -0500

    día 4 desafío resuelto

---
+++ dia4.js
@@ -0,0 +1,25 @@
+// Definimos el número secreto 
+let numeroSecreto = 7;
+
+// Inicializamos los intentos
+let intentos = 3;
+
+// Función para realizar el juego
+function adivinarNumero() {
+  while (intentos > 0) {
+    let adivina = parseInt(prompt(`Tienes ${intentos} intentos. Adivina el número secreto (entre 0 y 10):`));
+
+    if (adivina === numeroSecreto) {
+      alert("¡Felicidades! Adivinaste el número secreto.");
+      return;
+    } else {
+      alert("No es el número secreto.");
+      intentos--;
+    }
+  }
+
+  alert(`Lo siento, te has quedado sin intentos. El número secreto era: ${numeroSecreto}`);
+}
+
+// Llamamos a la función para empezar el juego
+adivinarNumero();
